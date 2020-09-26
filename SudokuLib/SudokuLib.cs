733d8149bb95990d95b05bcfab61b02a58538f8f
RoddyMac <rwmacrae@gmail.com>
2020-09-26 20:27:00 +0100

    Can't remember

---
+++ SudokuLib/SudokuLib.cs
@@ -0,0 +1,15 @@
+
+namespace SudokuLib
+{
+    public class Import
+    {
+        public static SudokuBoard FromCode(SudokuBoard sBoard)
+        {
+                
+                //Import here
+                sBoard.
+                return sBoard;
+        }
+        
+    }
+}
